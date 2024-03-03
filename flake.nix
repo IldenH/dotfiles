051d5a5cd04b20454c1ecc781f778f0ab11fb8a0
IldenH <IldenH.1@proton.me>
2024-03-03 14:04:29 +0100

    A bunch of stuff

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+          specialArgs = {inherit inputs;};
+          modules = [ 
+            ./configuration.nix
+          ];
+        };
+
+    };
+}
