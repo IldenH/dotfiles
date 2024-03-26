d405c24a9eaef18b11577d1b03b76725ba1a4556
IldenH <IldenH.1@proton.me>
2024-03-26 13:03:01 +0100

    Disabled vesktop crashing setting

--- home/programs/discord.nix
+++ home/programs/discord.nix
@@ -12,17 +12,18 @@
 				vesktop
 			];
 
-			home.file.".config/vesktop/settings.json".source = pkgs.writeText "settings.json" /* json */ ''
-			{
-				"discordBranch": "stable",
-				"minimizeToTray": false,
-				"arRPC": false,
-				"disableMinSize": true,
-				"staticTitle": true,
-				"splashTheming": true,
-				"checkUpdates": false
-			}
-			'';
+			# crahses vesktop
+			# home.file.".config/vesktop/settings.json".source = pkgs.writeText "settings.json" /* json */ ''
+			# {
+			# 	"discordBranch": "stable",
+			# 	"minimizeToTray": false,
+			# 	"arRPC": false,
+			# 	"disableMinSize": true,
+			# 	"staticTitle": true,
+			# 	"splashTheming": true,
+			# 	"checkUpdates": false
+			# }
+			# '';
 
 			home.file.".config/vesktop/settings/settings.json".source = ./discord-settings.json;
 
