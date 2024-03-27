8d9c95797f81d60ed91dadd70f721c24b8e7a181
IldenH <IldenH.1@proton.me>
2024-03-27 12:31:21 +0100

    More window rules

--- home/programs/wm/hyprland/rules.nix
+++ home/programs/wm/hyprland/rules.nix
@@ -2,9 +2,10 @@
 
 {
 	wayland.windowManager.hyprland.settings.windowrulev2 = [
-		# TODO: either make more window rules here, or just remove this
 		# "workspace 13, class:(vesktop)"
 		"workspace 3, class:(anki)"
-		"workspace 1, class:(steam)"
+		"workspace 4, class:(steam)"
+		"workspace 4, class:(heroic)"
+		"workspace 2, class:(firefox)" # TODO: different workspaces for different profiles
 	];
 }
