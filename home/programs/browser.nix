2e8d79fca04f65f98c55cb02e6e6c7c329b25c72
IldenH <IldenH.1@proton.me>
2024-03-27 14:50:13 +0100

    Changed firefox settings and cleaned browser.nix

--- home/programs/browser.nix
+++ home/programs/browser.nix
@@ -9,9 +9,7 @@
 			force = true;
 		};
 
-		# Settings are stored in ~/.mozilla/firefox/profile_name/prefs.js
-		# To find the name of a setting, either use `diff old_settings new_settings`
-		# Or check which value changes in about:config when setting it in about:preferences
+		# ~/.mozilla/firefox/profile_name/prefs.js
 		settingsWithHomepage = homepage: {
 			"browser.startup.homepage" = homepage;
 
@@ -33,17 +31,21 @@
 			"privacy.fingerprintingProtection.enabled" = true;
 			"privacy.globalprivacycontrol.enabled" = true;
 
+			# newtabpage
+			"browser.newtabpage.activity-stream.showSearch" = false;
+			"browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+
 			# no thanks mozilla
-			"app.shield.optoutstudies.enabled" = true;
+			"app.shield.optoutstudies.enabled" = false;
 			"datareporting.healthreport.uploadEnabled" = false;
 			"toolkit.telemetry.pioneer-new-studies-available" = false;
-			"browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
 			"browser.contentblocking.report.show_mobile_app" = false;
+			"extensions.pocket.enabled" = false;
 
 			# i don't know what is but probably good
 			"browser.shell.checkDefaultBrowser" = false;
 			"browser.shell.defaultBrowserCheckCount" = 1;
-			"toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+			"toolkit.legacyUserProfileCustomizations.stylesheets" = true; # makes userchrome work
 			"browser.startup.homepage_override.mstone" = "ignore";
 
 			"media.hardware-video-decoding.force-enabled" = true;
@@ -74,25 +76,79 @@
 			"browser.tabs.tabmanager.enabled" = false;
 			"browser.toolbars.bookmarks.visibility" = "newtab";
 
-			# extensions
-			"extensions.webextensions.ExtensionStorageIDB.migrated.@contain-facebook" = true;
-			"extensions.webextensions.ExtensionStorageIDB.migrated.addon@darkreader.org" = true;
-			"extensions.webextensions.ExtensionStorageIDB.migrated.sponsorBlocker@ajay.app" = true;
-			"extensions.webextensions.ExtensionStorageIDB.migrated.uBlock0@raymondhill.net" = true;
-			"extensions.webextensions.ExtensionStorageIDB.migrated.jid1-KKzOGWgsW3Ao4Q@jetpack" = true;
-			"extensions.webextensions.ExtensionStorageIDB.migrated.jid1-MnnxcxisBPnSXQ@jetpack" = true;
-			"extensions.webextensions.ExtensionStorageIDB.migrated.{446900e4-71c2-419f-a6a7-df9c091e268b}" = true;
-			"extensions.webextensions.ExtensionStorageIDB.migrated.{74145f27-f039-47ce-a470-a662b129930a}" = true;
-			"extensions.webextensions.ExtensionStorageIDB.migrated.{7a7a4a92-a2a0-41d1-9fd7-1e92480d612d}" = true;
-
 			# long string
-			"browser.uiCustomization.state" = 
-			''
-{"placements":{"widget-overflow-fixed-list":[],"unified-extensions-area":["jid1-mnnxcxisbpnsxq_jetpack-browser-action","_74145f27-f039-47ce-a470-a662b129930a_-browser-action","_contain-facebook-browser-action","dfyoutube_example_com-browser-action","jid1-kkzogwgsw3ao4q_jetpack-browser-action","addon_simplelogin-browser-action","sponsorblocker_ajay_app-browser-action","_34daeb50-c2d2-4f14-886a-7160b24d66a4_-browser-action","_9a41dee2-b924-4161-a971-7fb35c053a4a_-browser-action"],"nav-bar":["back-button","forward-button","stop-reload-button","urlbar-container","downloads-button","fxa-toolbar-menu-button","addon_darkreader_org-browser-action","_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action","ublock0_raymondhill_net-browser-action","_7a7a4a92-a2a0-41d1-9fd7-1e92480d612d_-browser-action","unified-extensions-button"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":[]},"seen":["save-to-pocket-button","developer-button","_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action","_74145f27-f039-47ce-a470-a662b129930a_-browser-action","addon_darkreader_org-browser-action","dfyoutube_example_com-browser-action","_9a41dee2-b924-4161-a971-7fb35c053a4a_-browser-action","_contain-facebook-browser-action","jid1-kkzogwgsw3ao4q_jetpack-browser-action","jid1-mnnxcxisbpnsxq_jetpack-browser-action","addon_simplelogin-browser-action","sponsorblocker_ajay_app-browser-action","_7a7a4a92-a2a0-41d1-9fd7-1e92480d612d_-browser-action","ublock0_raymondhill_net-browser-action","_34daeb50-c2d2-4f14-886a-7160b24d66a4_-browser-action"],"dirtyAreaCache":["nav-bar","PersonalToolbar","unified-extensions-area","TabsToolbar"],"currentVersion":20,"newElementCount":4}
+			"browser.uiCustomization.state" = ''
+			{
+			  "placements": {
+			    "widget-overflow-fixed-list": [],
+			    "unified-extensions-area": [
+			      "_6b733b82-9261-47ee-a595-2dda294a4d08_-browser-action",
+			      "jid1-mnnxcxisbpnsxq_jetpack-browser-action",
+			      "_74145f27-f039-47ce-a470-a662b129930a_-browser-action",
+			      "_contain-facebook-browser-action",
+			      "dfyoutube_example_com-browser-action",
+			      "jid1-kkzogwgsw3ao4q_jetpack-browser-action",
+			      "addon_simplelogin-browser-action",
+			      "sponsorblocker_ajay_app-browser-action",
+			      "_34daeb50-c2d2-4f14-886a-7160b24d66a4_-browser-action",
+			      "_9a41dee2-b924-4161-a971-7fb35c053a4a_-browser-action"
+			    ],
+			    "nav-bar": [
+			      "back-button",
+			      "forward-button",
+			      "stop-reload-button",
+			      "urlbar-container",
+			      "downloads-button",
+			      "fxa-toolbar-menu-button",
+			      "addon_darkreader_org-browser-action",
+			      "_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action",
+			      "ublock0_raymondhill_net-browser-action",
+			      "_7a7a4a92-a2a0-41d1-9fd7-1e92480d612d_-browser-action",
+			      "unified-extensions-button"
+			    ],
+			    "toolbar-menubar": [
+			      "menubar-items"
+			    ],
+			    "TabsToolbar": [
+			      "tabbrowser-tabs",
+			      "new-tab-button",
+			      "alltabs-button"
+			    ],
+			    "PersonalToolbar": [
+			      "personal-bookmarks"
+			    ]
+			  },
+			  "seen": [
+			    "save-to-pocket-button",
+			    "developer-button",
+			    "_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action",
+			    "_74145f27-f039-47ce-a470-a662b129930a_-browser-action",
+			    "addon_darkreader_org-browser-action",
+			    "dfyoutube_example_com-browser-action",
+			    "_9a41dee2-b924-4161-a971-7fb35c053a4a_-browser-action",
+			    "_contain-facebook-browser-action",
+			    "jid1-kkzogwgsw3ao4q_jetpack-browser-action",
+			    "jid1-mnnxcxisbpnsxq_jetpack-browser-action",
+			    "addon_simplelogin-browser-action",
+			    "sponsorblocker_ajay_app-browser-action",
+			    "_7a7a4a92-a2a0-41d1-9fd7-1e92480d612d_-browser-action",
+			    "ublock0_raymondhill_net-browser-action",
+			    "_34daeb50-c2d2-4f14-886a-7160b24d66a4_-browser-action",
+			    "_6b733b82-9261-47ee-a595-2dda294a4d08_-browser-action"
+			  ],
+			  "dirtyAreaCache": [
+			    "nav-bar",
+			    "PersonalToolbar",
+			    "unified-extensions-area",
+			    "TabsToolbar",
+			    "toolbar-menubar"
+			  ],
+			  "currentVersion": 20,
+			  "newElementCount": 6
+			}
 			'';
 		};
 
-		# All available extensions:
 		# https://gitlab.com/rycee/nur-expressions/-/blob/master/pkgs/firefox-addons/addons.json?ref_type=heads
 		extensions = with inputs.firefox-addons.packages."${pkgs.system}"; [
 			bitwarden
@@ -113,17 +169,6 @@
 
 			# firefox-color # nix-colors?
 		];
-
-		# lilleaila/dotfiles
-		colorTab = bg: fg: ''
-		.tab-background[selected] {
-			background-color: #${bg} !important;
-			background-image: none !important;
-		}
-		.tab-content[selected] {
-			color: #${fg} !important;
-		}
-		'';
 	in lib.mkIf (config.settings.browser.enable) {
 		programs.firefox = {
 			enable = true;
@@ -134,7 +179,6 @@
 				isDefault = true;
 				id = 0;
 				bookmarks = [ ];
-				userChrome = with config.colorScheme.palette; colorTab base04 base00;
 			};
 			profiles.school = {
 				settings = settingsWithHomepage "https://classroom.google.com";
@@ -172,7 +216,6 @@
 						}
 					];
 				}];
-				userChrome = with config.colorScheme.palette; colorTab base0C base00;
 			};
 			profiles.math = {
 				settings = settingsWithHomepage "https://skole.digilaer.no/auth/saml/index.php?wantsurl=https%3A%2F%2Fskole.digilaer.no%2Fmy%2F";
@@ -189,7 +232,6 @@
 						{ name = "Symbolab"; url = "https://symbolab.com"; keyword = "symbo"; }
 					];
 				}];
-				userChrome = with config.colorScheme.palette; colorTab base0D base00;
 			};
 		};
 
