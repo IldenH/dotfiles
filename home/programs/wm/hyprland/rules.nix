15aaee2cbbc528f3a54006bb7d94129834ac8e89
IldenH <IldenH.1@proton.me>
2024-04-02 20:22:57 +0200

    Qalculate is specialworkspace

--- home/programs/wm/hyprland/rules.nix
+++ home/programs/wm/hyprland/rules.nix
@@ -7,5 +7,9 @@
 		"workspace 4, class:(steam)"
 		"workspace 4, class:(heroic)"
 		"workspace 2, class:(firefox)" # TODO: different workspaces for different profiles
+
+		# special workspaces
+    "float,class:(qalculate-gtk)"
+    "workspace special:calculator,class:(qalculate-gtk)"
 	];
 }
