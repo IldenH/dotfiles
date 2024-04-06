6d7d11729f4bedce08d1d0c3592bcf85bc461728
IldenH <IldenH.1@proton.me>
2024-04-06 13:52:39 +0200

    Updated anki windowrule

--- home/programs/wm/hyprland/rules.nix
+++ home/programs/wm/hyprland/rules.nix
@@ -3,7 +3,7 @@
 {
 	wayland.windowManager.hyprland.settings.windowrulev2 = [
 		"workspace 13, class:(vesktop)"
-		"workspace 3, class:(anki)"
+		"workspace 3, class:(Anki)"
 		"workspace 4, class:(steam)"
 		"workspace 4, class:(heroic)"
 		"workspace 2, class:(firefox)" # TODO: different workspaces for different profiles
