bed32d93edd8d8407b0d5c95606514ea4ab709f6
IldenH <IldenH.1@proton.me>
2024-04-07 13:05:13 +0200

    Anki windowrules

--- home/programs/wm/hyprland/rules.nix
+++ home/programs/wm/hyprland/rules.nix
@@ -3,13 +3,17 @@
 {
 	wayland.windowManager.hyprland.settings.windowrulev2 = [
 		"workspace 13, class:(vesktop)"
-		"workspace 3, class:(Anki)"
 		"workspace 4, class:(steam)"
 		"workspace 4, class:(heroic)"
 		"workspace 2, class:(firefox)" # TODO: different workspaces for different profiles
 
+		# Anki
+		"workspace 3, class:(Anki)"
+		"center 1, class:(Anki), title:(Statistics|Preferences)"
+		"float, class:(Anki), title:(Statistics|Preferences)"
+
 		# special workspaces
-    "float,class:(qalculate-gtk)"
-    "workspace special:calculator,class:(qalculate-gtk)"
+    "float, class:(qalculate-gtk)"
+    "workspace special:calculator, class:(qalculate-gtk)"
 	];
 }
