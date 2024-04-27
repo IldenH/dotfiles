9cde8d81f54bb0652ada615b2995ba07e852796f
IldenH <IldenH.1@proton.me>
2024-04-27 08:13:48 +0200

    Discord: tweaked theme

--- home/programs/discord/theme.nix
+++ home/programs/discord/theme.nix
@@ -1,6 +1,5 @@
 { config, global }:
 
-
 # Auto-generated color scheme, inspired by https://github.com/deathbeam/base16-discord
 with config.colorScheme.palette; ''
 	/**
@@ -9,39 +8,45 @@ with config.colorScheme.palette; ''
 	* @version 1.0.0
 	* @description Base16 color scheme generated from https://github.com/Misterio77/nix-colors
 	**/
+
+  @import url(https://mwittrien.github.io/BetterDiscordAddons/Themes/SettingsModal/SettingsModal.css);
 	
 	:root {
-	--primary-630: #${base00}; /* Autocomplete background */
-	--primary-660: #${base00}; /* Search input background */
+		/* settings modal */
+		--settingswidth: 960;
+		--settingsheight: 80;
+		--settingsbackground: transparent;
+
+		--primary-630: #${base00}; /* Autocomplete background */
+		--primary-660: #${base00}; /* Search input background */
 	}
 	
 	.theme-light, .theme-dark {
-	--search-popout-option-fade: none; /* Disable fade for search popout */
-	--bg-overlay-2: #${base00}; /* These 2 are needed for proper threads coloring */
-	--home-background: #${base00};
-	--background-primary: #${base00};
-	--background-secondary: #${base00};
-	--background-secondary-alt: #${base00};
-	--channeltextarea-background: #${base01};
-	--background-tertiary: #${base00};
-	--background-accent: #${base0E};
-	--background-floating: #${base00};
-	--background-modifier-selected: #${base00};
-	--text-normal: #${base05};
-	--text-secondary: #${base00};
-	--text-muted: #${base03};
-	--text-link: #${base0C};
-	--interactive-normal: #${base04};
-	--interactive-hover: #${base0A};
-	--interactive-active: #${base0A};
-	--interactive-muted: #${base03};
-	--header-primary: #${base0E};
-	--header-secondary: #${base04};
-	--scrollbar-thin-track: transparent;
-	--scrollbar-auto-track: transparent;
+		--search-popout-option-fade: none; /* Disable fade for search popout */
+		--bg-overlay-2: #${base00}; /* These 2 are needed for proper threads coloring */
+		--home-background: #${base00};
+		--background-primary: #${base00};
+		--background-secondary: #${base00};
+		--background-secondary-alt: #${base00};
+		--channeltextarea-background: #${base01};
+		--background-tertiary: #${base00};
+		--background-accent: #${base0E};
+		--background-floating: #${base00};
+		--background-modifier-selected: #${base00};
+		--text-normal: #${base05};
+		--text-secondary: #${base00};
+		--text-muted: #${base03};
+		--text-link: #${base0C};
+		--interactive-normal: #${base04};
+		--interactive-hover: #${base0A};
+		--interactive-active: #${base0A};
+		--interactive-muted: #${base03};
+		--header-primary: #${base0E};
+		--header-secondary: #${base04};
+		--scrollbar-thin-track: transparent;
+		--scrollbar-auto-track: transparent;
 	}
 
-  /* code blocks */
   code {
     border-radius: 6px !important;
   }
@@ -50,29 +55,40 @@ with config.colorScheme.palette; ''
     padding: 3px !important;
   }
 
-	/* message input padding */
+	/* message input */
 	.scrollableContainer_ff917f {
-		padding-right: 10px !important;
+		padding-right: 0px !important;
 	}
 
-	/* hide stuff */
-	/* profile banner background color */
+  /* name titlebar */
+  .title_d4ba1a {
+    padding: 0 !important;
+  }
+
+  /* top-bar separator */
+  .searchBar__621ec {
+    box-shadow: none !important;
+  }
+
 	.banner__6d414, .panelBanner__7d7e2, .bannerPremium__69560 {
 		background-color: transparent !important;
 		background-image: none !important;
 	}
 
-	/* scrollbar */
 	.scroller_e412fe {
 		scrollbar-width: none !important;
 	}
 
-  /* message input buttons */
-	/* these are locale based */
+  /* message input buttons, locale based */
   button[aria-label="ギフトを贈る"],
   button[aria-label="GIFピッカーを開く"],
   button[aria-label="スタンプピッカーを開く"],
   .channelTextArea-1FufC0 > .container-1ZA19X,
+
+  /* top-bar buttons, locale based */
+  a[href="https://support.discord.com"],
+  [aria-label="ビデオ通話を開始"],
+  [aria-label="受信ボックス"],
 
   /* message input square */
   .form_d8a4a1::after,
@@ -83,17 +99,10 @@ with config.colorScheme.palette; ''
   a[href="/shop"],
   a[href="/store"],
 
-	/* nicknames text */
   .divider_bdb894, .akaBadge__27cd4, .nicknames__12efb,
 
   /* server list separator */
-  .guildSeparator__75928,
-
-  /* top-bar buttons */
-	/* also locale based */
-  a[href="https://support.discord.com"],
-  [aria-label="ビデオ通話を開始"],
-  [aria-label="受信ボックス"] {
+  .guildSeparator__75928 {
     display: none !important;
   }
 ''
