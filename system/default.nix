d04f7dc6338cc2e78719d07058dbeef73bcb1dc6
IldenH <IldenH.1@proton.me>
2024-05-01 11:46:40 +0200

    Utils: I am stupid

--- system/default.nix
+++ system/default.nix
@@ -14,7 +14,7 @@
 	];
 
 	options.settings = {
-		utils.enable = lib.mkEnableOption "utils";
+		utils.enable = lib.mkEnableOption "utils" // { default = true; };
 		sound.enable = lib.mkEnableOption "sound";
 		shell = lib.mkOption {
 			type = lib.types.package;
