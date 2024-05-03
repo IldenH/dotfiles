e1aa0f7edf6c0f381a142030d2586cabd7de3139
IldenH <IldenH.1@proton.me>
2024-05-03 18:49:31 +0200

    Games: gamemode and scope to system

--- home/programs/games.nix
+++ home/programs/games.nix
@@ -3,15 +3,12 @@
 {
 	options.settings.games.enable = lib.mkEnableOption "games";
 
+	# should probably just merge this into some other file TODO
 	config = lib.mkIf (config.settings.games.enable) {
 		home.packages = with pkgs; [
-			gamemode
-			gamescope
 			heroic
 			# lutris
 			prismlauncher
 		];
-
-		programs.mangohud.enable = true;
 	};
 }
--- system/steam.nix
+++ system/steam.nix
@@ -2,12 +2,24 @@
 
 {
 	options.settings.steam.enable = lib.mkEnableOption "steam";
+	options.settings.games.enable = lib.mkEnableOption "games";
 
-	config = lib.mkIf (config.settings.steam.enable) {
-		programs.steam = {
-		  enable = true;
-		  remotePlay.openFirewall = true;
-		  dedicatedServer.openFirewall = true;
-		};
-	};
+	config = lib.mkMerge [
+		(lib.mkIf (config.settings.steam.enable) {
+			programs.steam = {
+			  enable = true;
+			  remotePlay.openFirewall = true;
+			  dedicatedServer.openFirewall = true;
+				gamescopeSession.enable = true;
+			};
+
+			settings.games.enable = true;
+		})
+		(lib.mkIf (config.settings.games.enable) {
+			programs = {
+				gamemode.enable = true;
+				gamescope.enable = true;
+			};
+		})
+	];
 }
