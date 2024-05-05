d27e6dd72fa03d644bcc7eb6607928e35dd84c97
IldenH <IldenH.1@proton.me>
2024-05-05 13:33:49 +0200

    Hyprland: tweaked binds

--- home/programs/wm/hyprland/workspaces.nix
+++ home/programs/wm/hyprland/workspaces.nix
@@ -18,17 +18,11 @@
 		];
 
 		bind = [
-			", F1, workspace, 1"
-			", F2, workspace, 2"
-			", F3, workspace, 3"
-			", F4, workspace, 4"
-			", F5, workspace, 5"
-
-			"SHIFT, F1, workspace, 11"
-			"SHIFT, F2, workspace, 12"
-			"SHIFT, F3, workspace, 13"
-			"SHIFT, F4, workspace, 14"
-			"SHIFT, F5, workspace, 15"
+			"SUPER, 1, workspace, 1"
+			"SUPER, 2, workspace, 2"
+			"SUPER, 3, workspace, 3"
+			"SUPER, 4, workspace, 4"
+			"SUPER, 5, workspace, 5"
 
 			"SUPER, F1, workspace, 11"
 			"SUPER, F2, workspace, 12"
@@ -36,29 +30,17 @@
 			"SUPER, F4, workspace, 14"
 			"SUPER, F5, workspace, 15"
 
-			"CTRL, F1, movetoworkspace, 1"
-			"CTRL, F2, movetoworkspace, 2"
-			"CTRL, F3, movetoworkspace, 3"
-			"CTRL, F4, movetoworkspace, 4"
-			"CTRL, F5, movetoworkspace, 5"
+			"SUPER ALT, 1, movetoworkspace, 1"
+			"SUPER ALT, 2, movetoworkspace, 2"
+			"SUPER ALT, 3, movetoworkspace, 3"
+			"SUPER ALT, 4, movetoworkspace, 4"
+			"SUPER ALT, 5, movetoworkspace, 5"
 
-			"ALT, F1, movetoworkspace, 1"
-			"ALT, F2, movetoworkspace, 2"
-			"ALT, F3, movetoworkspace, 3"
-			"ALT, F4, movetoworkspace, 4"
-			"ALT, F5, movetoworkspace, 5"
-
-			"CTRL SHIFT, F1, movetoworkspace, 11"
-			"CTRL SHIFT, F2, movetoworkspace, 12"
-			"CTRL SHIFT, F3, movetoworkspace, 13"
-			"CTRL SHIFT, F4, movetoworkspace, 14"
-			"CTRL SHIFT, F5, movetoworkspace, 15"
-
-			"ALT SUPER, F1, movetoworkspace, 11"
-			"ALT SUPER, F2, movetoworkspace, 12"
-			"ALT SUPER, F3, movetoworkspace, 13"
-			"ALT SUPER, F4, movetoworkspace, 14"
-			"ALT SUPER, F5, movetoworkspace, 15"
+			"SUPER ALT, F1, movetoworkspace, 11"
+			"SUPER ALT, F2, movetoworkspace, 12"
+			"SUPER ALT, F3, movetoworkspace, 13"
+			"SUPER ALT, F4, movetoworkspace, 14"
+			"SUPER ALT, F5, movetoworkspace, 15"
 		];
 	};
 }
