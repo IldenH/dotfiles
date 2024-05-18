d24186570ce9c8ab1e294bab518f114b45f86e7d
IldenH <IldenH.1@proton.me>
2024-05-18 10:39:29 +0200

    Shell: merged cli.nix to default.nix

--- home/programs/shell/default.nix
+++ home/programs/shell/default.nix
@@ -1,9 +1,76 @@
-{...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   imports = [
-    ./cli.nix
     ./neofetch.nix
     ./neovim.nix
     ./terminal.nix
     ./zsh.nix
   ];
+  options.settings.terminal.cli.enable = lib.mkEnableOption "cli";
+
+  config = lib.mkIf (config.settings.terminal.cli.enable) {
+    home.packages = with pkgs; [
+      tldr
+      sops
+    ];
+
+    programs.git = {
+      enable = true;
+      userName = "IldenH";
+      userEmail = "IldenH.1" + "@" + "proton.me";
+      extraConfig = {
+        init.defaultBranch = "main";
+        credential.helper = "store"; # I should probably switch to ssh
+      };
+    };
+
+    programs.bat = {
+      enable = true;
+      config.theme = "base16";
+    };
+
+    programs.eza = {
+      enable = true;
+      git = true;
+      icons = true;
+      extraOptions = [
+        "--color=always"
+        "--group-directories-first"
+        "--header"
+        "--no-quotes"
+        "--hyperlink"
+        # "--total-size" # takes awhile to run
+      ];
+    };
+
+    programs.fzf = {
+      enable = true;
+      defaultOptions = ["--color 16"];
+    };
+
+    programs.direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
+
+    programs = {
+      ripgrep.enable = true;
+      thefuck.enable = true;
+      zoxide.enable = true;
+    };
+
+    home.shellAliases = {
+      cat = "bat";
+      cd = "z";
+      cdi = "zi";
+      cls = "clear && neofetch && ls";
+
+      makkergruppe = ''
+        cat ~/Documents/T1/makkergruppe-nr2 | wl-copy && wl-paste && echo "Copied :)"'';
+    };
+  };
 }
