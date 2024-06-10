99c40065694ace77b6393a0657af15756c7b487e
IldenH <IldenH.1@proton.me>
2024-06-10 13:40:49 +0200

    Hyprland: anki japanese window rules

--- home/wm/hyprland/rules.nix
+++ home/wm/hyprland/rules.nix
@@ -1,14 +1,15 @@
 _: {
-  wayland.windowManager.hyprland.settings.windowrulev2 = [
+  wayland.windowManager.hyprland.settings.windowrulev2 = let
+    anki = "class:(Anki), title:(Statistics|統計|Preferences|設定|Options|オプション)";
+  in [
     "workspace 13, class:(vesktop)"
     "workspace 4, class:(steam)"
     "workspace 4, class:(heroic)"
     "workspace 4, class:(org.prismlauncher.PrismLauncher)"
 
-    # anki
-    "center 1, class:(Anki), title:(Statistics|Preferences|Options)"
-    "size 70% 80%, class:(Anki), title:(Statistics|Preferences|Options)"
-    "float, class:(Anki), title:(Statistics|Preferences|Options)"
+    "center 1, ${anki}"
+    "size 70% 80%, ${anki}"
+    "float, ${anki}"
 
     # special workspaces
     "float, class:(qalculate-gtk)"
