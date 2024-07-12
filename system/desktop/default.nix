b12b0a7649da8b33c77e68c0aaa50f3e680e3876
IldenH <IldenH.1@proton.me>
2024-07-12 23:39:08 +0200

    System: gtk xdg portal

--- system/desktop/default.nix
+++ system/desktop/default.nix
@@ -1,4 +1,8 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   imports = [
     ./firefox.nix
     ./games.nix
@@ -8,4 +12,5 @@
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
   programs.hyprland.enable = config.hm.settings.hyprland.enable;
   security.pam.services.hyprlock = {};
+  xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
 }
