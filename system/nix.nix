9d60883bf1e5ce77a425eb7201d8717df98b63c2
IldenH <IldenH.1@proton.me>
2024-07-18 21:37:25 +0200

    Nix: settings and disable channels

--- system/nix.nix
+++ system/nix.nix
@@ -11,13 +11,25 @@
     programs.command-not-found.dbPath = inputs.programsdb.packages.${pkgs.system}.programs-sqlite;
 
     nix = {
-      settings.experimental-features = ["nix-command" "flakes"];
+      settings = {
+        experimental-features = ["nix-command" "flakes"];
+
+        warn-dirty = false;
+        eval-cache = false;
+        keep-going = true;
+
+        use-xdg-base-directories = true;
+
+        # pure-eval = true; # TODO: figure out what this does
+      };
 
       registry.nixpkgs.flake = inputs.nixpkgs;
       nixPath = [
         "nixpkgs=/etc/nixpkgs/channels/nixpkgs"
         "/nix/var/nix/profiles/per-user/root/channels"
       ];
+
+      channel.enable = false;
     };
 
     systemd.tmpfiles.rules = [
