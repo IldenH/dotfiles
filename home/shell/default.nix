52f40ee8733333ba88ef55149c153c61d03ea0c9
IldenH <IldenH.1@proton.me>
2024-11-07 20:27:42 +0100

    Flake: update inputs

--- home/shell/default.nix
+++ home/shell/default.nix
@@ -29,7 +29,7 @@
     programs.eza = {
       enable = true;
       git = true;
-      icons = true;
+      icons = "always";
       extraOptions = [
         "--color=always"
         "--group-directories-first"
