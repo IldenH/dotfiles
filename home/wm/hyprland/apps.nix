573e5a50f4ff5a32f8b45891974498a14a440fed
IldenH <IldenH.1@proton.me>
2025-01-15 21:11:52 +0100

    Hyprland: remove unused binds

--- home/wm/hyprland/apps.nix
+++ home/wm/hyprland/apps.nix
@@ -16,7 +16,6 @@ _: {
       "SUPER, P, exec, hyprlock"
 
       "SUPER, M, exec, $discord"
-      "SUPER ALT, M, exec, steam"
 
       "SUPER, B, exec, [workspace 3] $anki"
 
@@ -24,16 +23,12 @@ _: {
       "SUPER ALT, X, exec, prismlauncher"
 
       "SUPER, G, exec, $calculator"
-      "SUPER ALT, G, exec, geogebra"
-      "SUPER ALT CTRL, G, exec, krita"
 
       "SUPER ALT, R, exec, $screenrec"
 
       # browser
       "SUPER, N, exec, [workspace 2] $browser -P main --name main"
-      "SUPER ALT, N, exec, [workspace 4] $browser -P school --name school"
-      "SUPER, Y, exec, [workspace 4] $browser -P math --name math"
-      "SUPER ALT, Y, exec, [workspace 4] $browser -P math https://skole.digilaer.no/auth/saml/index.php?wantsurl=https%3A%2F%2Fskole.digilaer.no%2Fmod%2Fadobeconnect%2Fview.php%3Fid%3D13352 --name math"
+      "SUPER ALT, N, exec, [workspace 12] $browser -P school --name school"
     ];
   };
 }
