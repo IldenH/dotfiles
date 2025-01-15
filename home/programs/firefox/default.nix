5b692872400a966cb336a6ee5b101b65a0b5855c
IldenH <IldenH.1@proton.me>
2025-01-15 22:19:38 +0100

    Firefox: clean profiles

--- home/programs/firefox/default.nix
+++ home/programs/firefox/default.nix
@@ -38,12 +38,7 @@
           bookmarks = [];
         };
         school = {
-          settings =
-            settings
-            // {
-              "browser.startup.homepage" = "https://classroom.google.com";
-            };
-          inherit search userChrome userContent;
+          inherit settings search userChrome userContent;
           extensions = with inputs.firefox-addons.packages."${pkgs.system}"; [
             darkreader
             ublock-origin
@@ -55,7 +50,7 @@
               toolbar = true;
               bookmarks = [
                 {
-                  name = "Google";
+                  name = "School";
                   bookmarks = [
                     {
                       name = "Classroom";
@@ -82,103 +77,12 @@
                       url = "https://drive.google.com";
                       keyword = "drive";
                     }
-                  ];
-                }
-                {
-                  name = "Documents";
-                  bookmarks = [
                     {
-                      name = "English";
-                      url = "https://docs.google.com/document/d/1fquGUpLmUMMek7DTHdjvQruMavTfr6tJNmIAH4druRg";
-                      keyword = "english";
-                    }
-                    {
-                      name = "Nynorsk";
-                      url = "https://docs.google.com/document/d/1w73sIh5D19GLCMwNcnVr6RWT3P1tJR-vYhTuWIchLkc";
-                      keyword = "nynorsk";
-                    }
-                    {
-                      name = "Norsk";
-                      url = "https://docs.google.com/document/d/1vxERv5v3p_bf29W4lFLYCYrb6lmTmE0BA3fEVk_05jo";
-                      keyword = "norsk";
-                    }
-                    {
-                      name = "KRLE";
-                      url = "https://docs.google.com/document/d/1AR2khZxuxV4t5EArsW4xIxmMiq-3avlJM-Zl4uy7dGE";
-                      keyword = "krle";
-                    }
-                    {
-                      name = "Samfunnsfag";
-                      url = "https://docs.google.com/document/d/1_J0YtBoR9KBSlwHcbBORV32CBMkZhl10xZ2jVr4E5sU";
-                      keyword = "samfunns";
-                    }
-                    {
-                      name = "Deutsch";
-                      url = "https://docs.google.com/document/d/1B1fZ6ozgUKeJ1kYfpuq8-YH7cV8x8hQc5hCpgaxRqGU";
-                      keyword = "deutsch";
-                    }
-                    {
-                      name = "Musikk";
-                      url = "https://docs.google.com/document/d/1TY0RT-0pBqRoE8mT_cL9bUKUdR-t2a8gh6nD4UYoir4";
-                      keyword = "musikk";
-                    }
-                    {
-                      name = "Matte";
-                      url = "https://docs.google.com/document/d/1p1y7etzXHJvmSr4ZiIopA3aYYVOxBGBDfHsplutWOCs";
-                      keyword = "matte";
-                    }
-                    {
-                      name = "Naturfag";
-                      url = "https://docs.google.com/document/d/1AJx89t6wR48OjouMeb2w8H284AcMhrOZ_TifLaRCNgo";
-                      keyword = "natur";
-                    }
-                    {
-                      name = "K&H";
-                      url = "https://docs.google.com/document/d/17_xxjVTDNgnStCVm6uKiXDd2FZzGwInBvICP-YcmF1E";
-                      keyword = "kunst";
+                      name = "Office";
+                      url = "https://office.com";
+                      keyword = "office";
                     }
                   ];
-                }
-              ];
-            }
-          ];
-        };
-        math = {
-          settings =
-            settings
-            // {
-              "browser.startup.homepage" = "https://skole.digilaer.no/auth/saml/index.php?wantsurl=https%3A%2F%2Fskole.digilaer.no%2Fmy%2F";
-            };
-          inherit search userChrome userContent;
-          extensions = with inputs.firefox-addons.packages."${pkgs.system}"; [
-            darkreader
-            ublock-origin
-          ];
-          id = 2;
-          bookmarks = [
-            {
-              name = "Toolbar";
-              toolbar = true;
-              bookmarks = [
-                {
-                  name = "Digilær";
-                  url = "https://skole.digilaer.no/auth/saml/index.php?wantsurl=https%3A%2F%2Fskole.digilaer.no%2Fmy%2F";
-                  keyword = "digilær";
-                }
-                {
-                  name = "Digilær Møte";
-                  url = "https://skole.digilaer.no/auth/saml/index.php?wantsurl=https%3A%2F%2Fskole.digilaer.no%2Fmod%2Fadobeconnect%2Fview.php%3Fid%3D13352";
-                  keyword = "møte";
-                }
-                {
-                  name = "GeoGebra";
-                  url = "https://geogebra.org/classic";
-                  keyword = "geogebra";
-                }
-                {
-                  name = "Symbolab";
-                  url = "https://symbolab.com";
-                  keyword = "symbo";
                 }
               ];
             }
